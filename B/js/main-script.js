e8e6b07307ab13c15f7db699f4f0afd8e859a633
José Miguel Sequeira Pires de Azevedo Cutileiro <ist199097@i5-064.tagus.ist.utl.pt>
2023-05-15 09:41:12 +0100

    feat: add rotation cube example

--- B/js/main-script.js
+++ B/js/main-script.js
@@ -1,6 +1,9 @@
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
+var camera, scene, renderer;
+
+var geometry, material, cube;
 
 
 /////////////////////
@@ -8,13 +11,21 @@
 /////////////////////
 function createScene(){
     'use strict';
-
+      // create a scene
+      scene = new THREE.Scene();
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
-
+function createCameras() {
+    'use strict';
+    // create a camera
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    
+    camera.position.z = 5;
+    camera.lookAt(0, 0, 0);
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -45,7 +56,6 @@ function handleCollisions(){
 ////////////
 function update(){
     'use strict';
-
 }
 
 /////////////
@@ -53,23 +63,42 @@ function update(){
 /////////////
 function render() {
     'use strict';
-
 }
+
 
 ////////////////////////////////
 /* INITIALIZE ANIMATION CYCLE */
 ////////////////////////////////
 function init() {
     'use strict';
+    createScene();
+    createCameras();
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // create a cube
+    geometry = new THREE.BoxGeometry();
+    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    // position the camera
+    camera.position.z = 5;
 
 }
+  
 
 /////////////////////
 /* ANIMATION CYCLE */
 /////////////////////
 function animate() {
     'use strict';
-
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
 }
 
 ////////////////////////////
@@ -77,7 +106,6 @@ function animate() {
 ////////////////////////////
 function onResize() { 
     'use strict';
-
 }
 
 ///////////////////////
@@ -93,5 +121,4 @@ function onKeyDown(e) {
 ///////////////////////
 function onKeyUp(e){
     'use strict';
-
 }
