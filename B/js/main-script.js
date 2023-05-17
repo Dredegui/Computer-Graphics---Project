6487283f6be7d2ad99b9e5761a2810562ea51439
JoseCutileiro <jos3ndo@gmail.com>
2023-05-17 18:27:38 +0100

    feat: Trailor movement

--- B/js/main-script.js
+++ B/js/main-script.js
@@ -1,23 +1,35 @@
+const SPEED = 1;
+
 /*global THREE, requestAnimationFrame, console*/
 
 var camera, scene, renderer;
 var cameras = [];
 var geometry, material, mesh;
 
-var ball;
+var trailor,ball;
 
-function addTableLeg(obj, x, y, z) {
+// TRAILOR INFO
+var trailor_velX = [0,0]
+var trailor_velZ = [0,0]
+
+function addWheel(obj, x, y, z) {
     'use strict';
 
-    geometry = new THREE.CubeGeometry(2, 6, 2);
+    material = new THREE.MeshBasicMaterial({ color: 0xC41274, wireframe: true });
+
+    var Container =  new THREE.Object3D();
+    geometry = new THREE.CylinderGeometry(5,5, 6, 16);
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y - 3, z);
-    obj.add(mesh);
+
+    Container.add(mesh);
+    Container.rotation.x = 1.57;
+    Container.position.set(x, y-3, z);
+    obj.add(Container);
 }
 
-function addTableTop(obj, x, y, z) {
+function addCargo(obj, x, y, z) {
     'use strict';
-    geometry = new THREE.CubeGeometry(60, 2, 20);
+    geometry = new THREE.CubeGeometry(60, 20, 20);
     mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(x, y, z);
     obj.add(mesh);
@@ -40,24 +52,26 @@ function createBall(x, y, z) {
 }
 
 
-function createTable(x, y, z) {
+function createTrailer(x, y, z) {
     'use strict';
 
-    var table = new THREE.Object3D();
+    trailor = new THREE.Object3D();
 
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+    material = new THREE.MeshBasicMaterial({ color: 0x14EA44, wireframe: true });
 
-    addTableTop(table, 0, 0, 0);
-    addTableLeg(table, -25, -1, -8);
-    addTableLeg(table, -25, -1, 8);
-    addTableLeg(table, 25, -1, 8);
-    addTableLeg(table, 25, -1, -8);
+    addCargo(trailor, 0, 10, 0);
+    /*
+    addWheel(trailor, -25, -1, -8);
+    addWheel(trailor, -25, -1, 8);
+    addWheel(trailor, 25, -1, 8);
+    addWheel(trailor, 25, -1, -8);
+    */
 
-    scene.add(table);
+    scene.add(trailor);
 
-    table.position.x = x;
-    table.position.y = y;
-    table.position.z = z;
+    trailor.position.x = x;
+    trailor.position.y = y;
+    trailor.position.z = z;
 }
 
 function createScene() {
@@ -68,7 +82,7 @@ function createScene() {
 
     scene.add(new THREE.AxisHelper(10));
 
-    createTable(0, 8, 0);
+    createTrailer(0, 8, 0);
     createBall(0, 0, 15);
 }
 
@@ -123,8 +137,8 @@ function createCameras() {
 
     cameras[4] = createCamera(PERSPECTIVE,50,50,50);
 
-    camera = cameras[0];
-
+    //INITIAL CAMERA
+    camera = cameras[4];
 }
 
 function onResize() {
@@ -146,9 +160,29 @@ function onKeyDown(e) {
     // Camera control (generic)
     if (e.keyCode <= 53 && e.keyCode >= 49) {
         camera = cameras[e.keyCode - 49];
+        return ;
     }
 
     switch (e.keyCode) {
+
+
+        case 37: // LEFT ARROW
+            trailor_velX[0] = -1;
+            break;
+
+        case 38: // UP ARROW
+            trailor_velZ[0] = -1;
+            break;
+
+        case 39: // RIGHT ARROW
+            trailor_velX[1] = 1;
+            break;
+
+            
+        case 40: // DOWN ARROW
+            trailor_velZ[1] = 1;
+            break;
+
         case 65: //A
         case 97: //a
             scene.traverse(function (node) {
@@ -174,6 +208,26 @@ function onKeyDown(e) {
     }
 }
 
+function onKeyUp(e) {
+    'use strict';
+
+    switch (e.keyCode) {
+        case 37: // LEFT ARROW
+            trailor_velX[0] = 0; // Stop left movement when key is released
+            break;
+        case 38: // LEFT ARROW
+            trailor_velZ[0] = 0; // Stop left movement when key is released
+            break;
+        case 39: // RIGHT ARROW
+            trailor_velX[1] = 0; // Stop right movement when key is released
+            break;
+        case 40: // LEFT ARROW
+            trailor_velZ[1] = 0; // Stop left movement when key is released
+            break;
+    }
+}
+
+
 function render() {
     'use strict';
     renderer.render(scene, camera);
@@ -193,6 +247,7 @@ function init() {
     render();
 
     window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
     window.addEventListener("resize", onResize);
 }
 
@@ -204,6 +259,20 @@ function animate() {
         ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
         ball.position.z = 15 * (Math.cos(ball.userData.step));
     }
+
+
+    // Normalizado
+    var newX = trailor_velX[0] + trailor_velX[1];
+    var newZ = trailor_velZ[0] + trailor_velZ[1];
+
+    if (newX != 0 && newZ != 0) {
+        newX = newX / Math.sqrt(2);
+        newZ = newZ / Math.sqrt(2);
+    }
+
+    trailor.position.x += newX * SPEED;
+    trailor.position.z += newZ * SPEED;
+
     render();
 
     requestAnimationFrame(animate);
