4db343f752a2bdbc5967762c49a51528940b2f65
JoseCutileiro <jos3ndo@gmail.com>
2023-05-19 13:24:00 +0100

    feat: Head and upperBody of the robot

--- B/js/main-script.js
+++ B/js/main-script.js
@@ -1,16 +1,30 @@
 const SPEED = 1;
+const SCALE = 3;
 
-/*global THREE, requestAnimationFrame, console*/
-
+////////////////////////////////////////////
+/////////         BASE          ////////////
+////////////////////////////////////////////
 var camera, scene, renderer;
 var cameras = [];
 var geometry, material, mesh;
 
-var trailor,ball;
 
-// TRAILOR INFO
+////////////////////////////////////////////
+/////////         TRAILOR          /////////
+////////////////////////////////////////////
+var trailor;
 var trailor_velX = [0,0]
 var trailor_velZ = [0,0]
+
+////////////////////////////////////////////
+/////////         ROBOT            /////////
+////////////////////////////////////////////
+var robot, pivotHead;
+var head_ror = [0,0];
+var i = 0;
+
+
+
 
 function addWheel(obj, x, y, z) {
     'use strict';
@@ -18,37 +32,95 @@ function addWheel(obj, x, y, z) {
     material = new THREE.MeshBasicMaterial({ color: 0xC41274, wireframe: true });
 
     var Container =  new THREE.Object3D();
-    geometry = new THREE.CylinderGeometry(5,5, 6, 16);
+    geometry = new THREE.CylinderGeometry(SCALE * 1.5,SCALE * 1.5, SCALE * 1.5, 16);
     mesh = new THREE.Mesh(geometry, material);
 
     Container.add(mesh);
-    Container.rotation.x = 1.57;
+    // Rotate 90º
+    Container.rotation.z = 1.57;
     Container.position.set(x, y-3, z);
     obj.add(Container);
 }
 
 function addCargo(obj, x, y, z) {
     'use strict';
-    geometry = new THREE.CubeGeometry(60, 20, 20);
+    geometry = new THREE.CubeGeometry(20, 20, 60);
+    material = new THREE.MeshBasicMaterial({ color: 0x1b2133, wireframe: true });
     mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(x, y, z);
     obj.add(mesh);
 }
 
-function createBall(x, y, z) {
+function addConnection(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CubeGeometry(15, 5, 25);
+    material = new THREE.MeshBasicMaterial({ color: 0x344F24, wireframe: true });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+
+function addWaist(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CubeGeometry(6 * SCALE, 2 * SCALE, 4 * SCALE);
+    material = new THREE.MeshBasicMaterial({ color: 0x2b3113, wireframe: true });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function addGrill(obj, x, y, z) {
+    'use strict';
+    geometry = new THREE.CubeGeometry(4 * SCALE, 4 * SCALE, 1 * SCALE);
+    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+function addUpperBody(obj, x, y, z) {
+
     'use strict';
 
-    ball = new THREE.Object3D();
-    ball.userData = { jumping: true, step: 0 };
+    var UpperBody = new THREE.Object3D();
 
-    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-    geometry = new THREE.SphereGeometry(4, 10, 10);
+    addWaist(UpperBody,x,y - 4 * SCALE,z);
+    addGrill(UpperBody,x,y - SCALE * 2,z + 2 * SCALE);
+
+    addWheel(UpperBody,x - 2.2*SCALE,y - SCALE * 4,z);
+    addWheel(UpperBody,x + 2.2*SCALE,y - SCALE * 4,z);
+
+    geometry = new THREE.CubeGeometry(6 * SCALE, 6 * SCALE, 4 * SCALE);
+    material = new THREE.MeshBasicMaterial({ color: 0x1b2133, wireframe: true });
     mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
 
-    ball.add(mesh);
-    ball.position.set(x, y, z);
+    UpperBody.add(mesh);
 
-    scene.add(ball);
+    obj.add(UpperBody);
+}
+
+function addHead(obj, x, y, z) {
+
+    'use strict';
+
+    var Head = new THREE.Object3D();
+
+    geometry = new THREE.CubeGeometry(2 * SCALE, 1.5 * SCALE, 2 * SCALE);
+    material = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0, 2*SCALE,0);
+
+    Head.add(mesh);
+
+    pivotHead = new THREE.Object3D();
+    pivotHead.position.set(x,y+SCALE,z);
+    pivotHead.add(mesh);
+    scene.add(pivotHead);
+
+
+    obj.add(Head);
 }
 
 
@@ -57,21 +129,58 @@ function createTrailer(x, y, z) {
 
     trailor = new THREE.Object3D();
 
-    material = new THREE.MeshBasicMaterial({ color: 0x14EA44, wireframe: true });
 
     addCargo(trailor, 0, 10, 0);
-    /*
-    addWheel(trailor, -25, -1, -8);
-    addWheel(trailor, -25, -1, 8);
-    addWheel(trailor, 25, -1, 8);
-    addWheel(trailor, 25, -1, -8);
-    */
+
+
+    addConnection(trailor,0,3,35);
+
+    addWheel(trailor, -8, -1, -25);
+    addWheel(trailor, 8, -1, -25);
+    addWheel(trailor, 8, -1, 25);
+    addWheel(trailor, -8, -1, 25);
+
 
     scene.add(trailor);
 
     trailor.position.x = x;
     trailor.position.y = y;
     trailor.position.z = z;
+}
+
+function createRobot(x, y, z) {
+    'use strict';
+
+    robot = new THREE.Object3D();
+
+    // tronco
+    addUpperBody(robot, 0, 10, 0);
+
+
+    // cabeça
+    addHead(robot,0,SCALE * 3.5 + 10,0);
+
+    /*
+    // left leg
+    addLeg(robot, -25, -1, -8);
+    
+    // Right leg
+    addLeg(robot, -25, -1, -8);
+
+    // Left arm
+    addArm(robot, -25, -1, -8);
+
+    // Right arm
+    addArm(robot, -25, -1, -8);
+    */
+
+
+
+    scene.add(robot);
+
+    robot.position.x = x;
+    robot.position.y = y;
+    robot.position.z = z;
 }
 
 function createScene() {
@@ -81,9 +190,10 @@ function createScene() {
 
 
     scene.add(new THREE.AxisHelper(10));
+    scene.background = new THREE.Color( 0xffffff );
 
-    createTrailer(0, 8, 0);
-    createBall(0, 0, 15);
+    createTrailer(-50, 8, -50);
+    createRobot(0, 8, 0);
 }
 
 const ORTOGONAL = 0;
@@ -183,6 +293,16 @@ function onKeyDown(e) {
             trailor_velZ[1] = 1;
             break;
 
+        case 70: // F
+        case 102: // f
+            head_ror[0] = -1;
+            break;
+
+        case 82: // R
+        case 114: // r
+            head_ror[1] = 1;
+            break;
+
         case 65: //A
         case 97: //a
             scene.traverse(function (node) {
@@ -194,7 +314,7 @@ function onKeyDown(e) {
 
         case 83:  //S
         case 115: //s
-            ball.userData.jumping = !ball.userData.jumping;
+            // ball.userData.jumping = !ball.userData.jumping;
             break;
 
         case 69:  //E
@@ -254,17 +374,11 @@ function init() {
 function animate() {
     'use strict';
 
-    if (ball.userData.jumping) {
-        ball.userData.step += 0.04;
-        ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
-        ball.position.z = 15 * (Math.cos(ball.userData.step));
-    }
-
-
-    // Normalizado
+    // Movimentar o reboque
     var newX = trailor_velX[0] + trailor_velX[1];
     var newZ = trailor_velZ[0] + trailor_velZ[1];
 
+    // Normalizar
     if (newX != 0 && newZ != 0) {
         newX = newX / Math.sqrt(2);
         newZ = newZ / Math.sqrt(2);
@@ -272,6 +386,10 @@ function animate() {
 
     trailor.position.x += newX * SPEED;
     trailor.position.z += newZ * SPEED;
+
+    // Rodar a cabeça
+    i += 0.01;
+    pivotHead.rotation.set(i,0,0);
 
     render();
 
